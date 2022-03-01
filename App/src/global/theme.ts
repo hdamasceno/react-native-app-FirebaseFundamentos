26cb47f7592f86025d3200bc075c6ee9aada03e5
hdamasceno <hdamasceno@gmail.com>
2022-02-28 23:47:06 -0300

    BugFix->Removidos estilos nao utilizados

--- App/src/global/theme.ts
+++ App/src/global/theme.ts
@@ -4,16 +4,8 @@ export default {
         white: '#FFF',
         green: '#34A853',
         blue: '#4285F4',
-        gray1: '#E3E3E5',
-        gray2: '#E9E9EB',
         red: '#EB4335',
-        background: '#FAFAFC',
-        primary: '#FBBC05',
 
-        textHeading: '#585666',
-        confirmationButton: '#FF941A',
-        cancelButtonTitle: '#666666',
-        brandSecondary: '#585666',
         TailWind: {
             Slate: {
                 50: '#f8fafc',
